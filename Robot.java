8b60c186018053ccab47749bfe79e0d47f51dd35
Avery Whitaker <averywhitaker@gmail.com>
2015-01-02 15:32:17 -0500

    Create Robot.java

---
+++ Robot.java
@@ -0,0 +1,39 @@
+package org.pihisamurai;
+
+//Missing imports
+
+public class Robot extends SimpleRobot {
+	// Common Code
+	public Drivetrain drivetrain = new Drivetrain(this);
+	public Manipulator manipulator = new Manipulator(this);
+
+	// Modes
+	public Teleoperated teleoperated = new Teleoperated(this);
+	public Test test = new Test(this);
+	public Autonomous autonomous = new Autonomous(this);
+
+	public Gamepad gamepad = new Gamepad(1);
+
+	public void autonomous() {
+		System.out.println("Autonomous Mode");
+		autonomous.run();
+	}
+
+	public void operatorControl() {
+		System.out.println("Teleoperated Mode");
+		teleoperated.run();
+	}
+
+	public void test() {
+		System.out.println("Test Mode");
+		test.run();
+	}
+
+	public void sleep(long milis) {
+		try {
+			Thread.sleep(milis);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
