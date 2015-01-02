edfdaa17f51d4c669c57e99a8090ffd4f912f0a4
Avery Whitaker <averywhitaker@gmail.com>
2015-01-02 15:32:40 -0500

    Create Teleoperated.java

---
+++ Teleoperated.java
@@ -0,0 +1,22 @@
+package org.pihisamurai;
+
+public class Teleoperated implements RobotMode {
+	private Robot robot;
+	public HumanControl humanControl;
+	public DriverStationGUI GUI;
+
+	public Teleoperated(Robot r) {
+		this.robot = r;
+		humanControl = new HumanControl(robot);
+		GUI = new DriverStationGUI(robot);
+	}
+
+	public void run() {
+		while (true) {
+			GUI.update();
+			robot.drivetrain.setSpeed(humanControl.getLeftSpeed(),
+					humanControl.getRightSpeed());
+			robot.sleep(10);
+		}
+	}
+}
