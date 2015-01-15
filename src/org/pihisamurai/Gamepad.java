57992ceac1ef87bc64ae8ac63e2f15ffd6d00ab2
Avery Whitaker <averywhitaker@gmail.com>
2015-01-14 22:02:35 -0500

    Added comment reccomending changes

--- src/org/pihisamurai/Gamepad.java
+++ src/org/pihisamurai/Gamepad.java
@@ -5,72 +5,92 @@
  */
 
 package org.pihisamurai;
-import edu.wpi.first.wpilibj.Joystick; 
-import edu.wpi.first.wpilibj.DriverStation; 
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.DriverStation;
 
 /**
  *
  * @author PiHi Samurai 1076
  */
 
-public class Gamepad extends Joystick{
+/*
+ * Averys Note 1/14/14
+ * 
+ * I recommend re-writing it to contain a joystick rather then extend one as to
+ * make it easier to use
+ * 
+ * http://first.wpi.edu/FRC/roborio/release/docs/java/
+ * classedu_1_1wpi_1_1first_1_1wpilibj_1_1Joystick.html
+ * 
+ * It should contain a function to get every button, get the x/y of the sticks,
+ * get values of throttles, and get directional pad direction
+ * 
+ * Right now it uses constants and a get button function, lets change those
+ * constants to be private and have a specific function for each button
+ * 
+ * It should add the rumble functionality
+ */
 
-    static final int LEFT_BUMPER = 5;
-    static final int RIGHT_BUMPER = 6;
-    static final int LEFT_TRIGGER = 7;
-    static final int RIGHT_TRIGGER = 8;    
-    
-    static final int F310_A = 1;
-    static final int F310_B = 2;
-    static final int F310_X = 3;
-    static final int F310_Y = 4;
-    static final int F310_LB = 5;
-    static final int F310_RB = 6;
-    static final int F310_L_BACK = 7;
-    static final int F310_R_START = 8;
-    
-    static final int F310_LEFT_Y = 2;
-    static final int F310_LEFT_X = 1;
-    static final int F310_RIGHT_Y = 5;
-    static final int F310_RIGHT_X = 4;
-    static final int F310_TRIGGER_AXIS = 3;
-    static final int F310_DPAD_X_AXIS = 6;
-    
-    int port;
-    DriverStation driverStation;
-    Gamepad(int port){
-        super(port);
-        this.port = port;
-        driverStation = DriverStation.getInstance();
-    }
-    
-    double getLeftX(){
-        return getRawAxis(F310_LEFT_X);
-    }
+public class Gamepad extends Joystick {
 
-    double getLeftY(){
-        return getRawAxis(F310_LEFT_Y);
-    }
+	static final int LEFT_BUMPER = 5;
+	static final int RIGHT_BUMPER = 6;
+	static final int LEFT_TRIGGER = 7; // AGAIN MAY HAVE CHANGED
+	static final int RIGHT_TRIGGER = 8;
 
-    double getRightX(){
-        return getRawAxis(F310_RIGHT_X);
-    }
+	static final int F310_A = 1;
+	static final int F310_B = 2;
+	static final int F310_X = 3;
+	static final int F310_Y = 4;
+	static final int F310_LB = 5;
+	static final int F310_RB = 6;
+	static final int F310_L_BACK = 7;
+	static final int F310_R_START = 8;
 
-    double getRightY(){
-        return getRawAxis(F310_RIGHT_Y);
-    }
+	static final int F310_LEFT_Y = 2;
+	static final int F310_LEFT_X = 1; // THEESE MAY HAVE CHANGED
+	static final int F310_RIGHT_Y = 5;
+	static final int F310_RIGHT_X = 4;
+	static final int F310_TRIGGER_AXIS = 3;
+	static final int F310_DPAD_X_AXIS = 6;
 
-    public double getRawAxis(int axis){
-            return driverStation.getStickAxis(port, axis);
-    }
-    
-    //These aren't called anywhere in this project.
-   boolean getNumberedButton(int button){
-        return ((0x1 << (button-1)) & driverStation.getStickButtons(port)) != 0;
-    }
-    
-    public boolean getButtonA(){
-        return getNumberedButton(1);
-    }
+	int port;
+	DriverStation driverStation;
+
+	Gamepad(int port) {
+		super(port);
+		this.port = port;
+		driverStation = DriverStation.getInstance();
+	}
+
+	double getLeftX() {
+		return getRawAxis(F310_LEFT_X);
+	}
+
+	double getLeftY() {
+		return getRawAxis(F310_LEFT_Y);
+	}
+
+	double getRightX() {
+		return getRawAxis(F310_RIGHT_X);
+	}
+
+	double getRightY() {
+		return getRawAxis(F310_RIGHT_Y);
+	}
+
+	public double getRawAxis(int axis) {
+		return driverStation.getStickAxis(port, axis);
+	}
+
+	// No idea why this is needed with getRawButton() which is part of joystick
+	boolean getNumberedButton(int button) {
+		return ((0x1 << (button - 1)) & driverStation.getStickButtons(port)) != 0;
+	}
+
+	public boolean getButtonA() {
+		return getNumberedButton(1);
+	}
 
 }
