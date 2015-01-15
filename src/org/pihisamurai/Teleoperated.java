d628440dbbeb95942fbcc064efba9e2a52705219
Avery Whitaker <averywhitaker@gmail.com>
2015-01-14 21:42:22 -0500

    Removed HumanControl.java
    It served same function as gamepad

--- src/org/pihisamurai/Teleoperated.java
+++ src/org/pihisamurai/Teleoperated.java
@@ -6,12 +6,10 @@ import edu.wpi.first.wpilibj.Jaguar;
 public class Teleoperated implements RobotMode {
 
 	private Robot robot;
-	public HumanControl humanControl;
 	public DriverStationGUI GUI;
 
 	public Teleoperated(Robot r) {
 		this.robot = r;
-		humanControl = new HumanControl(robot);
 		GUI = new DriverStationGUI(robot);
 	}
 	
