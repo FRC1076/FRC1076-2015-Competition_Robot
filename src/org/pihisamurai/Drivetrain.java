d68d970a2012ad9011d103508f22f3f0de2beaee
avery-whitaker <averywhitker@gmail.com>
2015-01-23 08:56:35 -0500

    Added distance based alternative PID

--- src/org/pihisamurai/Drivetrain.java
+++ src/org/pihisamurai/Drivetrain.java
@@ -8,100 +8,160 @@ import edu.wpi.first.wpilibj.PIDSource;
 
 public class Drivetrain {
 
-	private static double P = 1;
-	private static double I = 0;
-	private static double D = 0;
-	private static double F = 0;
+	private static final double	P							= 1;
+	private static final double	I							= 0;
+	private static final double	D							= 0;
+	private static final double	F							= 0;
 
-	private static double DISTANCE_PER_PULSE_MAIN = 4*16 / 128; //Circumference of wheel, modified by gear ratio divided by number of pulses in one rotation of encoder
-	private static double DISTANCE_PER_PULSE_STRAFE = 4*16 / 128; //Circumference of wheel, modified by gear ratio divided by number of pulses in one rotation of encoder
-	
-	Encoder leftEncoder;
-	Encoder rightEncoder;
-	Encoder strafeEncoder;
-	
-	Jaguar backLeftMotor;
-	Jaguar frontLeftMotor;
-	Jaguar backRightMotor;
-	Jaguar frontRightMotor;
-	Jaguar StrafeMotor;
-	Jaguar LiftMotor;
-	
-	PIDController leftPIDControl;
-	PIDController rightPIDControl;
-	PIDController strafePIDControl;
+	private static final int	LEFT_ENCODER_CHANNEL_A		= 0;
+	private static final int	LEFT_ENCODER_CHANNEL_B		= 1;
 
-	private Robot robot;
+	private static final int	RIGHT_ENCODER_CHANNEL_A		= 0;
+	private static final int	RIGHT_ENCODER_CHANNEL_B		= 1;
+
+	private static final int	STRAFE_ENCODER_CHANNEL_A	= 0;
+	private static final int	STRAFE_ENCODER_CHANNEL_B	= 1;
+
+	// Circumference of wheel, modified by gear ratio
+	// divided by number of
+	// pulses in one rotation of encoder
+	private static final double	DISTANCE_PER_PULSE_MAIN		= 4 * 16 / 128;
+	private static final double	DISTANCE_PER_PULSE_STRAFE	= 4 * 16 / 128;
+
+	Encoder						leftRateEncoder;
+	Encoder						rightRateEncoder;
+	Encoder						strafeRateEncoder;
+	Encoder						leftDistEncoder;
+	Encoder						rightDistEncoder;
+	Encoder						strafeDistEncoder;
+
+	Jaguar						backLeftMotor;
+	Jaguar						frontLeftMotor;
+	Jaguar						backRightMotor;
+	Jaguar						frontRightMotor;
+	Jaguar						StrafeMotor;
+	Jaguar						LiftMotor;
+
+	PIDController				leftPIDSpeedControl;
+	PIDController				rightPIDSpeedControl;
+	PIDController				strafePIDSpeedControl;
+
+	PIDController				leftPIDDistControl;
+	PIDController				rightPIDDistControl;
+	PIDController				strafePIDDistControl;
+
+	private Robot				robot;
 
 	public Drivetrain(Robot r) {
 		this.robot = r;
 
-		leftEncoder = new Encoder(0,1, true); //TODO ports, oreintation
-		rightEncoder = new Encoder(2,3, false);
-		strafeEncoder = new Encoder(2,3, false);
-		leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE_MAIN); //TODO placeholder
-		rightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE_MAIN);
-		strafeEncoder.setDistancePerPulse(DISTANCE_PER_PULSE_STRAFE);
-		leftEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kRate);
-		rightEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kRate);
-		strafeEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kRate);
-		
-		
-		
-		backLeftMotor = new Jaguar(0); // Changed ?? to 0 - 4
+		leftRateEncoder = new Encoder(LEFT_ENCODER_CHANNEL_A, LEFT_ENCODER_CHANNEL_B, true); // TODO
+																								// ports,
+																								// oreintation
+		rightRateEncoder = new Encoder(RIGHT_ENCODER_CHANNEL_A, RIGHT_ENCODER_CHANNEL_B, false);
+		strafeRateEncoder = new Encoder(STRAFE_ENCODER_CHANNEL_A, STRAFE_ENCODER_CHANNEL_B, false);
+		leftRateEncoder.setDistancePerPulse(DISTANCE_PER_PULSE_MAIN); // TODO
+																		// placeholder
+		rightRateEncoder.setDistancePerPulse(DISTANCE_PER_PULSE_MAIN);
+		strafeRateEncoder.setDistancePerPulse(DISTANCE_PER_PULSE_STRAFE);
+		leftRateEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kRate);
+		rightRateEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kRate);
+		strafeRateEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kRate);
+
+		leftDistEncoder = new Encoder(0, 1, true); // TODO
+													// ports,
+													// oreintation
+		rightDistEncoder = new Encoder(2, 3, false);
+		strafeDistEncoder = new Encoder(2, 3, false);
+		leftDistEncoder.setDistancePerPulse(DISTANCE_PER_PULSE_MAIN); // TODO
+																		// placeholder
+		rightDistEncoder.setDistancePerPulse(DISTANCE_PER_PULSE_MAIN);
+		strafeDistEncoder.setDistancePerPulse(DISTANCE_PER_PULSE_STRAFE);
+		leftDistEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kDistance);
+		rightDistEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kDistance);
+		strafeDistEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kDistance);
+
+		backLeftMotor = new Jaguar(0);
 		frontLeftMotor = new Jaguar(1);
 		backRightMotor = new Jaguar(2);
 		frontRightMotor = new Jaguar(3);
 		StrafeMotor = new Jaguar(4);
 		LiftMotor = new Jaguar(5);
 
-		PIDOutput leftMotors = new PIDOutput(){
+		PIDOutput leftMotors = new PIDOutput() {
 			public void pidWrite(double power) {
 				backLeftMotor.set(-power);
-				frontLeftMotor.set(-power); // May need to be opposite directions
+				frontLeftMotor.set(-power);
 			}
 		};
-		
-		PIDOutput rightMotors = new PIDOutput(){
-			public void pidWrite(double power) {
-				backLeftMotor.set(-power);
-				frontLeftMotor.set(-power); // May need to be opposite directions
-			}
-		};
-		
 
-		leftPIDControl = new PIDController(P, I, D, F, leftEncoder, leftMotors);
-		rightPIDControl = new PIDController(P, I, D, F, leftEncoder, rightMotors);
-		strafePIDControl = new PIDController(P, I, D, F, leftEncoder, StrafeMotor);
+		PIDOutput rightMotors = new PIDOutput() {
+			public void pidWrite(double power) {
+				backLeftMotor.set(power);
+				frontLeftMotor.set(power);
+			}
+		};
+
+		leftPIDSpeedControl = new PIDController(P, I, D, F, leftRateEncoder, leftMotors);
+		rightPIDSpeedControl = new PIDController(P, I, D, F, leftRateEncoder, rightMotors);
+		strafePIDSpeedControl = new PIDController(P, I, D, F, leftRateEncoder, StrafeMotor);
+
+		leftPIDDistControl = new PIDController(P, I, D, F, leftDistEncoder, leftMotors);
+		rightPIDDistControl = new PIDController(P, I, D, F, leftDistEncoder, rightMotors);
+		strafePIDDistControl = new PIDController(P, I, D, F, leftDistEncoder, StrafeMotor);
 	}
-	
-	public void setRightSpeed(double speed){
-		rightPIDControl.setSetpoint(speed);
+
+	public void setRightSpeed(double speed) {
+		rightPIDDistControl.disable();
+		rightPIDSpeedControl.enable();
+		rightPIDSpeedControl.setSetpoint(speed);
 	}
-	
-	public void setLeftSpeed(double speed){
-		leftPIDControl.setSetpoint(speed);
+
+	public void setLeftSpeed(double speed) {
+		leftPIDDistControl.disable();
+		leftPIDSpeedControl.enable();
+		leftPIDSpeedControl.setSetpoint(speed);
 	}
-	
-	public void setStrafeSpeed(double speed){
-		strafePIDControl.setSetpoint(speed);
+
+	public void setStrafeSpeed(double speed) {
+		strafePIDDistControl.disable();
+		strafePIDSpeedControl.enable();
+		strafePIDSpeedControl.setSetpoint(speed);
 	}
-	
+
+	public void setRightDist(double dist) {
+		rightPIDSpeedControl.disable();
+		rightPIDDistControl.enable();
+		rightPIDDistControl.setSetpoint(dist);
+	}
+
+	public void setLeftDist(double dist) {
+		leftPIDSpeedControl.disable();
+		leftPIDDistControl.enable();
+		leftPIDDistControl.setSetpoint(dist);
+	}
+
+	public void setStrafeDist(double dist) {
+		strafePIDSpeedControl.disable();
+		strafePIDDistControl.enable();
+		strafePIDDistControl.setSetpoint(dist);
+	}
+
 	/*
-
-	public void rightPower(double power) {
-		backRightMotor.set(power);
-		frontRightMotor.set(power); // May need to be opposite directions
-	}
-
-	public void leftPower(double power) {
-		backLeftMotor.set(-power);
-		frontLeftMotor.set(-power); // May need to be opposite directions
-	}
-
-	public void strafePower(double power) {
-		StrafeMotor.set(-power);
-	}*/
+	 * 
+	 * public void rightPower(double power) {
+	 * backRightMotor.set(power);
+	 * frontRightMotor.set(power); // May need to be
+	 * opposite directions }
+	 * 
+	 * public void leftPower(double power) {
+	 * backLeftMotor.set(-power);
+	 * frontLeftMotor.set(-power); // May need to be
+	 * opposite directions }
+	 * 
+	 * public void strafePower(double power) {
+	 * StrafeMotor.set(-power); }
+	 */
 
 	public void liftPower(double power) {
 		LiftMotor.set(power); // For lift motors
@@ -110,11 +170,13 @@ public class Drivetrain {
 	/*
 	 * Implement for Autonomous once encoders on robot
 	 * 
-	 * public void moveDistance(double distance, double speed){
+	 * public void moveDistance(double distance, double
+	 * speed){
 	 * 
 	 * }
 	 * 
-	 * public void moveArc(double radius, double angle, double speed){
+	 * public void moveArc(double radius, double angle,
+	 * double speed){
 	 * 
 	 * }
 	 * 
@@ -122,7 +184,8 @@ public class Drivetrain {
 	 * 
 	 * }
 	 * 
-	 * public void strafeDistance(double distance, double speed){
+	 * public void strafeDistance(double distance, double
+	 * speed){
 	 * 
 	 * }
 	 */
