fc97df8935e37e2e4b46ca270d0ba59a9d5fda64
claytonrie <jonfieldthe2nd@gmail.com>
2015-01-24 16:11:37 -0500

    Putting Lift motor in Manipulator

--- src/org/pihisamurai/Manipulator.java
+++ src/org/pihisamurai/Manipulator.java
@@ -3,9 +3,15 @@ package org.pihisamurai;
 public class Manipulator {
 
 	private Robot robot;
+	private Jaguar LiftMotor;
 
 	Manipulator(Robot r) {
 		this.robot = r;
+		
+		LiftMotor = new Jaguar(5);
 	}
-
+	
+	public void liftPower(double power) {
+		LiftMotor.set(power); // For lift motors
+	}
 }
