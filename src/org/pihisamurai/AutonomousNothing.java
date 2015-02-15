243afbf7bd66b0b7c3945693a6f4889458dd3ede
Avery Whitkaer <averywhitaker@gmail.com>
2015-02-15 16:44:46 -0500

    added Autonomous Nothing

---
+++ src/org/pihisamurai/AutonomousNothing.java
@@ -0,0 +1,19 @@
+package org.pihisamurai;
+
+public class AutonomousNothing {
+
+	Robot robot;
+
+	AutonomousNothing(Robot robot) {
+		this.robot = robot;
+	}
+	
+	public void run() {
+
+	}
+
+	public void init() {
+
+	}
+
+}
