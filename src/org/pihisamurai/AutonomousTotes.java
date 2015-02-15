e61004d1b12876aabbc5a0955fc09d0e14ffcd07
Avery Whitkaer <averywhitaker@gmail.com>
2015-02-15 16:45:06 -0500

    dded Autonomous totes frameworks

---
+++ src/org/pihisamurai/AutonomousTotes.java
@@ -0,0 +1,19 @@
+package org.pihisamurai;
+
+public class AutonomousTotes {
+
+	Robot robot;
+
+	AutonomousTotes(Robot robot) {
+		this.robot = robot;
+	}
+	
+	public void run() {
+
+	}
+
+	public void init() {
+
+	}
+
+}
