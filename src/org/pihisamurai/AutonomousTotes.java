812d82058f1ac260beb82afc0f37239047fb031c
Avery Whitkaer <averywhitaker@gmail.com>
2015-02-27 21:25:32 -0500

    Clayton's Anton

--- src/org/pihisamurai/AutonomousTotes.java
+++ src/org/pihisamurai/AutonomousTotes.java
@@ -5,100 +5,107 @@ public class AutonomousTotes {
 	Robot robot;
 	
 	private String program;
+	
+	Thread programmingParse;
 
 	AutonomousTotes(Robot robot) {
 		this.robot = robot;
-	}
+		programmingParse = new Thread(new Runnable () {
+			public void run () {
+				execute(parse(program));
+			}
 
-	public void run() {
-		execute(parse(program));
+			private int[] parse(String code) {
+				String[] lines = code.split("");
+				int[] list = new int[lines.length];
+				for (int i = 0; i < lines.length; i++) {
+					list[i] = -1;
+					try { 
+						list[i] = Integer.parseInt(lines[i]);
+					} catch (Exception e) {
+						list[i] = 10 + ";/fblrudp".indexOf(code);
+						if (list[i] == 9) {
+							list[i] = -1;
+						}
+					}
+				}
+				return list;
+			}
+
+			public void execute(int[] c) {
+				double[] stack = new double[12];
+				int pointer = 0;
+				for (int i = 0; i < c.length; i++) {
+					if(pointer < 1) {
+						pointer++;
+					}
+					if (c[i] < 10) {
+						stack[pointer] = Math.pow(10, stack[pointer]) + (c[i] / 10);
+					} else {
+						switch (c[i]) {
+						case 10: pointer++; break;
+						case 11:
+							stack[pointer] /= 10;
+							break;
+						case 12:
+							robot.drivetrain.goDistPrimary(stack[pointer], 0.5);
+							stack[pointer] = 0;
+							pointer--;
+							break;
+						case 13:
+							robot.drivetrain.goDistPrimary(stack[pointer], -0.5);
+							stack[pointer] = 0;
+							pointer--;
+							break;
+						case 14:
+							robot.drivetrain.goDistStrafe(stack[pointer], 0.5);
+							stack[pointer] = 0;
+							pointer--;
+							break;
+						case 15:
+							robot.drivetrain.goDistStrafe(stack[pointer], -0.5);
+							stack[pointer] = 0;
+							pointer--;
+							break;
+						case 16:
+							robot.manipulator.setLiftPower(0.5);
+							try {
+								Thread.sleep((long) (stack[pointer] * 1000));
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							stack[pointer] = 0;
+							pointer--;
+							break;
+						case 17:
+							robot.manipulator.setLiftPower(-0.5);
+							try {
+								Thread.sleep((long) (stack[pointer] * 1000));
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							stack[pointer] = 0;
+							pointer--;
+							break;
+						case 18:
+							stack[pointer] = 0;
+							pointer--;
+							break;
+						}
+						
+						robot.manipulator.setLiftPower(0);
+					}
+				}
+			}
+		});
 	}
 
 	public void init(String code) {
 		program = code;
+		programmingParse.start();
 	}
 
-	private int[] parse(String code) {
-		String[] lines = code.split("");
-		int[] list = new int[lines.length];
-		for (int i = 0; i < lines.length; i++) {
-			list[i] = -1;
-			try { 
-				list[i] = Integer.parseInt(lines[i]);
-			} catch (Exception e) {
-				switch (lines[i]) {
-				case ";": list[i] = 10; break;
-				case "/": list[i] = 11; break;
-				case "f": list[i] = 12; break;
-				case "b": list[i] = 13; break;
-				case "l": list[i] = 14; break;
-				case "r": list[i] = 15; break;
-				case "u": list[i] = 16; break;
-				case "d": list[i] = 17; break;
-				case "p": list[i] = 18; break;
-				default: break;
-				}
-			}
-		}
-		return list;
-	}
-
-	public void execute(int[] c) {
-		double[] stack = new double[12];
-		int pointer = 0;
-		for (int i = 0; i < c.length; i++) {
-			if(pointer < 1) {
-				pointer++;
-			}
-			if (c[i] < 10) {
-				stack[pointer] = Math.pow(10, stack[pointer]) + (c[i] / 10);
-			} else {
-				double time = System.nanoTime();
-				switch (c[i]) {
-				case 10: pointer++; break;
-				case 11:
-					stack[pointer] /= 10;
-					break;
-				case 12:
-					robot.drivetrain.goDistPrimary(stack[pointer], 0.5);
-					stack[pointer] = 0;
-					pointer--;
-					break;
-				case 13:
-					robot.drivetrain.goDistPrimary(stack[pointer], -0.5);
-					stack[pointer] = 0;
-					pointer--;
-					break;
-				case 14:
-					robot.drivetrain.goDistStrafe(stack[pointer], 0.5);
-					stack[pointer] = 0;
-					pointer--;
-					break;
-				case 15:
-					robot.drivetrain.goDistStrafe(stack[pointer], -0.5);
-					stack[pointer] = 0;
-					pointer--;
-					break;
-				case 16:
-					while ((System.nanoTime() - time) / 1000000000 > 10000 * stack[pointer]) {
-						robot.manipulator.setLiftPower(0.5);
-					}
-					stack[pointer] = 0;
-					pointer--;
-					break;
-				case 17:
-					while ((System.nanoTime() - time) / 1000000000 > 10000 * stack[pointer]) {
-						robot.manipulator.setLiftPower(-0.5);
-					}
-					stack[pointer] = 0;
-					pointer--;
-					break;
-				case 18:
-					stack[pointer] = 0;
-					pointer--;
-					break;
-				}
-			}
-		}
+	public void run () {
+		
 	}
 }
