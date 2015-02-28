5d32665fada73d7b2cdcb64f54ed936eda061d82
Avery Whitkaer <averywhitaker@gmail.com>
2015-02-27 21:27:06 -0500

    Update default speed config values

--- src/org/pihisamurai/SpeedController.java
+++ src/org/pihisamurai/SpeedController.java
@@ -10,13 +10,13 @@ public class SpeedController {
 	SpeedController(Robot robot){
 		this.robot = robot;
 		
-		SmartDashboard.putNumber("Turn Speed 0 Boxes", 5.1);
-		SmartDashboard.putNumber("Turn Speed 1 Boxes", 5.1);
-		SmartDashboard.putNumber("Turn Speed 2 Boxes", 4.5);
-		SmartDashboard.putNumber("Turn Speed 3 Boxes", 3.5);
-		SmartDashboard.putNumber("Turn Speed 4 Boxes", 2.5);
-		SmartDashboard.putNumber("Turn Speed 5 Boxes", 2.0);
-		SmartDashboard.putNumber("Turn Speed 6 Boxes", 1.5);
+		SmartDashboard.putNumber("Turn Speed 0 Boxes", 4.0);
+		SmartDashboard.putNumber("Turn Speed 1 Boxes", 4.0);
+		SmartDashboard.putNumber("Turn Speed 2 Boxes", 3.5);
+		SmartDashboard.putNumber("Turn Speed 3 Boxes", 3.0);
+		SmartDashboard.putNumber("Turn Speed 4 Boxes", 2.0);
+		SmartDashboard.putNumber("Turn Speed 5 Boxes", 1.5);
+		SmartDashboard.putNumber("Turn Speed 6 Boxes", 1.3);
 
 		SmartDashboard.putNumber("Stick change rate 0 boxes", 2.0);
 		SmartDashboard.putNumber("Stick change rate 1 boxes", 2.0);
@@ -41,7 +41,6 @@ public class SpeedController {
 	
 	public void setBoxCount(int box){
 		this.box = box;
-		SmartDashboard.putNumber("Box Count", box);
 	}
 	
 	double turnSpeedModifier = 1;
@@ -51,12 +50,15 @@ public class SpeedController {
 	}
 	
 	public void update(){
+
+		SmartDashboard.putNumber("Box Count", box);
+		
 		double turnSpeed =  SmartDashboard.getNumber("Turn Speed " + box + " Boxes");
 		double accelLimit =  SmartDashboard.getNumber("Stick change rate " + box + " boxes");
 		double turnAccelLimit = SmartDashboard.getNumber("Angle stick rate " + box + " boxes");
 
 		robot.drivetrain.setPrimaryLimit(accelLimit);
-		robot.drivetrain.setStrafeLimit(accelLimit);
+		robot.drivetrain.setStrafeLimit(999.0);
 		robot.drivetrain.setAngleAccelLimit(turnAccelLimit);
 		
 		robot.drivetrain.setMaxTurnSpeed(turnSpeed*turnSpeedModifier);
