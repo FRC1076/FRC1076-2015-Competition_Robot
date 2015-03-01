b58b099b63f1d5975eae97690cbe905ad94cb7cd
avery-whitaker <averywhitker@gmail.com>
2015-03-01 17:21:33 -0500

    orginized imports

--- src/org/pihisamurai/Drivetrain.java
+++ src/org/pihisamurai/Drivetrain.java
@@ -1,6 +1,5 @@
 package org.pihisamurai;
 
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Jaguar;
