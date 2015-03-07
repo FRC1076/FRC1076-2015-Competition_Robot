f626fd2be16966e24732da95c5ade7125a3b325c
avery-whitaker <averywhitker@gmail.com>
2015-03-06 23:58:40 -0500

    Revert "Updated Robot.java to use new gamepad recording"
    This reverts commit cbf5cf404b9c58232907ffb61c1fd74096f5c1e6.

--- src/org/pihisamurai/Robot.java
+++ src/org/pihisamurai/Robot.java
@@ -30,7 +30,6 @@ public class Robot extends IterativeRobot {
 
 		robot = this;
 		
-		SmartDashboard.putString("Gamepad Read File", "");
 		SmartDashboard.putNumber("Autonomous Mode", 1);
 
 		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
@@ -100,25 +99,17 @@ public class Robot extends IterativeRobot {
 		
 		modeStart = System.nanoTime();
 		
-		gamepad1 = new GamepadReplay(SmartDashboard.getString("Gamepad Read File") + "-driver.gamepad");
-		gamepad2 = new GamepadReplay(SmartDashboard.getString("Gamepad Read File") + "-operator.gamepad");
+		gamepad1 = new GamepadReplay(SmartDashboard.getString("Gamepad Read File 1"));
+		gamepad2 = new GamepadReplay(SmartDashboard.getString("Gamepad Read File 2"));
 		
-		
-		teleop.init();
-		
-		//autonomous.init((int)SmartDashboard.getNumber("Autonomous Mode"));
+		autonomous.init((int)SmartDashboard.getNumber("Autonomous Mode"));
 	}
 
 	// The function called roughly every twenty milliseconds during autonomous mode 
 
 	public void autonomousPeriodic() {
-		
-		teleop.run();
-		drivetrain.update();
-		gamepad1.update();
-		gamepad2.update();
-		
-		// autonomous.run();
+		autonomous.run();
+		// drivetrain.update();
 	}
 
 	// The initial function called on start of teleop
@@ -128,8 +119,8 @@ public class Robot extends IterativeRobot {
 
 		modeStart = System.nanoTime();
 		
-		gamepad1 = new GamepadReal(0, SmartDashboard.getString("Gamepad Read File") + "-driver.gamepad");
-		gamepad2 = new GamepadReal(1, SmartDashboard.getString("Gamepad Read File") + "-operator.gamepad");
+		gamepad1 = new GamepadReal(0, SmartDashboard.getString("Gamepad Write File 1"));
+		gamepad2 = new GamepadReal(1, SmartDashboard.getString("Gamepad Write File 2"));
 		
 		teleop.init();
 	}
